8831e8971df32cf6339b95c9cb037cfe81c3d531
manish <mg.sanderao@gmail.com>
2018-10-24 17:33:18 +0530

    #	src/java/BST/CommanFunction.java

--- src/java/BST/CommanFunction.java
+++ src/java/BST/CommanFunction.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 public class CommanFunction {
      void Print(String str){
@@ -22,28 +21,4 @@ public class CommanFunction {
 
 
 }
-=======
 
-public class CommanFunction {
-     void Print(String str){
-         System.out.print(str);
-     }
-     void printArray(int[] arr){
-         for (int i=0;i<arr.length;i++){
-             System.out.print(arr[i]+"_");
-         }
-     }
-     boolean CompareArr(int[] arr1,int[] arr2){
-         boolean flag = true;
-         for (int i = 0;i<arr1.length;i++){
-             if(arr1[i] != arr2[i]){
-                 flag = false;
-                 break;
-             }
-         }
-         return flag;
-     }
-
-
-}
->>>>>>> origin/master
