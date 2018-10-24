ccbf069cd16d1667a914fb740f2131c3b4fe4236
manish <mg.sanderao@gmail.com>
2018-10-24 17:10:46 +0530

    Changes

---
+++ src/java/BST/CommanFunction.java
@@ -0,0 +1,23 @@
+
+public class CommanFunction {
+     void Print(String str){
+         System.out.print(str);
+     }
+     void printArray(int[] arr){
+         for (int i=0;i<arr.length;i++){
+             System.out.print(arr[i]+"_");
+         }
+     }
+     boolean CompareArr(int[] arr1,int[] arr2){
+         boolean flag = true;
+         for (int i = 0;i<arr1.length;i++){
+             if(arr1[i] != arr2[i]){
+                 flag = false;
+                 break;
+             }
+         }
+         return flag;
+     }
+
+
+}
